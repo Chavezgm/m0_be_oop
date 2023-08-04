9b880af188323f176d4fc296d89e2d517c4615ed
Martin Chavez <martinchavez1203@gmail.com>
2023-08-04 13:31:11 -0600

    Hobbit prob

--- oop.rb
+++ oop.rb
@@ -51,6 +51,22 @@ end
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon 
+    attr_accessor :name,:rider,:color,:is_hungry,:times_eaten
+    def initialize(name, rider, color)
+    @name = name 
+    @rider = rider
+    @color = color
+    @is_hungry = true
+    @times_eaten = 0
+  end 
+  def Eat
+    @times_eaten += 1
+    if @times_eaten >= 4 
+      @is_hungry = false 
+    end 
+  end
+  end 
 
 
 #  Write a Hobbit class
@@ -61,3 +77,25 @@ end
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+    attr_accessor :name,:disposition,:age,:is_adult, :is_old,:has_ring
+    def initialize(name, disposition)
+      @name = name 
+      @disposition = disposition 
+      @age = 0
+      @is_adult =false 
+      @is_old = false
+      @has_ring = (name == "Frodo")
+    end 
+  
+    def celebrate_birthday
+      @age += 1
+      if @age >= 33 
+        @is_adult = true
+      end 
+      if @age >= 101
+        @is_old = true 
+      end 
+    end 
+  end 
+  
